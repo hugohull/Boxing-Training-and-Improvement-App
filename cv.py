1cd531e96dccbfe541fbbaf8c371420ecdba31ea
hugohull <96350325+hugohull@users.noreply.github.com>
2024-02-05 15:24:39 +0000

    Blue and red object tracking is functional

--- cv.py
+++ cv.py
@@ -26,6 +26,37 @@ while True:
     if not success:
         break
 
+    # Convert to HSV color space
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # Red color range
+    lower_red1 = np.array([170, 75, 50])
+    upper_red1 = np.array([180, 255, 255])
+
+    # Blue color range
+    lower_blue = np.array([100, 75, 50])
+    upper_blue = np.array([120, 255, 255])
+
+    # Create masks for red and blue
+    mask_red = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    # Find contours and draw them for red
+    contours_red, _ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours_red:
+        area = cv2.contourArea(cnt)
+        if area > 400:
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 3)
+
+    # Find contours and draw them for blue
+    contours_blue, _ = cv2.findContours(mask_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours_blue:
+        area = cv2.contourArea(cnt)
+        if area > 400:
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+
     # Add line
     cv2.line(img, START, END, COLOUR, THICKNESS)
 
