d03a4273faadcc364e8b62c063b1da9879e47830
hugohull <96350325+hugohull@users.noreply.github.com>
2024-03-30 13:58:04 +0000

    Removed excess code from VideoThread.py

--- VideoThread.py
+++ VideoThread.py
@@ -1,33 +1,3 @@
-# import cv2
-# from PyQt5.QtCore import QThread, pyqtSignal, Qt
-# from PyQt5.QtGui import QImage
-#
-# from punch_tracker import run_punch_tracker
-#
-#
-# class VideoThread(QThread):
-#     change_pixmap_signal = pyqtSignal(QImage)
-#
-#     def __init__(self, parent=None):
-#         super(VideoThread, self).__init__(parent)
-#         self.track_punches_flag = lambda: True
-#
-#     def run(self):
-#         self.running = True
-#         run_punch_tracker(self.update_frame, self.track_punches_flag)
-#
-#     def stop(self):
-#         self.running = False
-#         self.wait()
-#
-#     def update_frame(self, frame):
-#         # Convert frame to format suitable for QtGui
-#         rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#         h, w, ch = rgb_image.shape
-#         bytes_per_line = ch * w
-#         convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
-#         p = convert_to_Qt_format.scaled(640, 480, aspectRatioMode=Qt.KeepAspectRatio)
-#         self.change_pixmap_signal.emit(p)
 import cv2
 from PyQt5.QtCore import QThread, pyqtSignal, Qt
 from PyQt5.QtGui import QImage
